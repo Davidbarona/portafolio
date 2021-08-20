fe0c07e5a11f525c846ac788bb12e6735197a7de
unknown <davidb@soymomo.com>
2021-08-19 23:05:29 -0500

    create nabvar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import {Navbar} from './components/Navbar'
+
 
 function App() {
   return (
     <div className="App">
-      <h2>hola mundo</h2>
+      <Navbar/>
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import logo from '../img/Logo.png'
+export const Navbar = () => {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+          <div className="container">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img className='logoNavbar' src={logo} alt='img'/></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Sobre Mi</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">servicios</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">how work</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Portafolio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Contacto</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+    )
+}
